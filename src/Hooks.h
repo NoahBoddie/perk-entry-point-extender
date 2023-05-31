66f5aa073e5361b59330bd9770de08fea1da8f1a
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2023-05-31 12:34:38 -0400

    Added Apply AttackSpellsHook
    Enabled those with no weapons to use combat hit spell perk entries

--- src/Hooks.h
+++ src/Hooks.h
@@ -27,12 +27,50 @@ namespace PEPE
 	};
 	
 
+	struct ApplyAttackSpellsHook
+	{
+		static void  Patch()
+		{
+			SKSE::AllocTrampoline(14);
+
+			auto& trampoline = SKSE::GetTrampoline();
+
+			//SE: 628C20, AE: 660A70, VR: ???
+			auto hit_hook = REL::RelocationID(37673, 38627);
+			
+			func = trampoline.write_call<5>(hit_hook.address() + RELOCATION_OFFSET(0x185, 0x194), thunk);
+			
+			logger::info("ApplyAttackSpellsHook complete...");
+		}
+
+		//This hook is so ununique btw, that I think I can just write branch this shit. Straight up.
+		static void thunk(RE::Actor* a_this, RE::InventoryEntryData* entry_data, bool is_left, RE::Actor* target)
+		{
+			if (!entry_data)
+			{
+				//static auto* weap = ;
+				//Used to use new
+				static RE::InventoryEntryData temp_data{ RE::TESForm::LookupByID<RE::TESBoundObject>(0x1F4), 1 };
+				
+				func(a_this, &temp_data, is_left, target);
+				
+				//delete data;
+			}
+			else
+				func(a_this, entry_data, is_left, target);
+		}
+
+		static inline REL::Relocation<decltype(thunk)> func;
+	};
+
+
 	struct Hooks
 	{
 		static void Install()
 		{
 			//No need for allocation, YAAAAAY!
 			EntryPointPerkEntry__EvaluateConditionHook::Patch();
+			ApplyAttackSpellsHook::Patch();
 		}
 	};
 }
